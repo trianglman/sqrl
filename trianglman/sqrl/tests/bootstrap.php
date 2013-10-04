46b24016c4b6a9ba08bd6adb7c2b3e0b1a1c5c48
johnj <john.a.judy@gmail.com>
2013-10-03 20:39:31 -0500

    set up initial project structure
    Created a composer to bring in the QR image generating library and ensure that OpenSSL is installed. Created a src and interface directory and pair of objects to handle the processing. Added a test directory with a configuration and bootstrap.

---
+++ trianglman/sqrl/tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+function autoload($className)
+{
+    $base = dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..';
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    if ($lastNsPos = strripos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    require $base.DIRECTORY_SEPARATOR.$fileName;
+}
+spl_autoload_register('autoload');
